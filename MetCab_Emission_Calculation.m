564169bafa9f490d68215c84ceda81b662790ebc
Henrik Grythe <heg@heglux.nil0u.no>
2020-06-03 15:14:03 +0200

    Semi operational version of the model, added functionality to reading files and completed some test runs.

---
+++ MetCab_Emission_Calculation.m
@@ -0,0 +1,92 @@
+function a = MetCab_Emission_Calculation()
+
+%--------------------------------------------------------------------------
+% Part 2 Calculate consumption
+%--------------------------------------------------------------------------
+% Get emission factors (per Landsdel):
+
+% Get total Cabins per fylke
+for f=1:length(fnum)
+    in                 = extractfield(Cab,'FYLKE')==fnum(f);
+    Tot_Cab(1,fnum(f)) = nansum(extractfield(Shy(in),'bui2hol'))+nansum(extractfield(Shy(in),'bui2hut'));
+    Tot_Cab(2,fnum(f)) = nansum(extractfield(Shy(in),'bui2hut'));
+    Tot_Cab(3,fnum(f)) = nansum(extractfield(Shy(in),'bui2hol'));
+end
+
+%--------------------------------------------------------------------------
+% In here would go some part to assign differentiated consumption weights.
+%--------------------------------------------------------------------------
+
+% Use total Cabins per Landsdel to calculate consumption per hytte
+[Hy, ~, HyT]   = xlsread(EFfile,'Landsdel_To_Fylke');
+LDstr          = HyT(2:end,1);
+for LD=1:7
+    Fkes       = Hy(LD,~isnan(Hy(LD,:)));
+    Fylker     = Fkes(2:end);
+    L_Ncab(LD) = nansum(Tot_Cab(1,Fylker));
+end
+% Cons: Landsdel: 7   Tech   :  4    year: 12
+% Cons: Landsdel: 7   SPECIES: 13    year: 12
+yn = find(y==yyyy);
+T_Cons = squeeze(Cons(:,1,find(y==yyyy)));
+LEF    = squeeze(EF(:,:,find(y==yyyy)));
+% Consumption Per HYtte and convert do dry wood consumption only
+cphy   = 1e6*DryWoodFactor*T_Cons./L_Ncab';
+% Emission per hytte (UNIT= consumption [kg] * EmissonFactor [g/kg''])
+% ''Units vary for emission factors, look to emission factor input file.
+Ephy   = cphy.*LEF;
+
+% Parameter to split up the file into smaller managable pieces (to speed up)
+part   = round(linspace(1,length(Shy),500));
+
+% Snew Empty container for the cabins
+Snew   = [];
+
+[Ly, ~, LyT]    = xlsread(EFfile,'Fylke_To_Landsdel');
+
+% Loop through the parts
+for p=2:length(part)
+    
+    % the last part needs to be dealt with separately.
+    if p<length(part)
+        S=Shy(part(p-1):part(p)-1);
+    else
+        S=Shy(part(p-1):part(p));
+    end
+    % Calculate emissions and assign EMISSION fields to struct.
+    for i=1:length(S)
+        NK =  nansum(extractfield(S(i),'bui2hol'))+nansum(extractfield(S(i),'bui2hut'));
+        f  = extractfield(S(i),'FYLKE');
+        LD = Ly((Ly(:,1)==f),3);
+        EM = squeeze(Ephy(LD,:))*NK;
+        S  = setfield(S,{i},'Cons_kg',{1},cphy(LD)*NK); % Make a consumption field
+        % Loop over all species.
+        for j=1:size(SPEC,1)
+            S = setfield(S,{i},sprintf('%s_%i',char(SPEC(j)),yyyy),{1},EM(j));
+        end
+        %if rem(i,500)==0; disp(i); end
+    end
+    part(p)
+    Snew = cat(1,Snew,S);
+end
+
+% File of Hytter with kommune and fylkes ID
+fname     = sprintf('%sCabin_Emissions_%04i',pname,yyyy);
+dbfspec   = makedbfspec(Snew);
+shapewrite(Snew, fname, 'DbfSpec', dbfspec)
+pfilename = strcat(fname,'.prj');
+fid       = fopen(pfilename,'w+');
+fprintf(fid,'%s',prj);
+fclose(fid);
+fprintf('Wrote file  %s \n',fname)
+
+[CHy, ~, CHyT]  = xlsread(EFfile,'9704-3');
+LC              = CHy(2:4:end,:);
+yr              = CHy(1,:);
+Landsdel        = HyT(2:end,1);
+
+
+for i=1:length(Landsdel)
+    fprintf('%18s : %04i Consumption per Cabin %5.1f (N:%6i) \n',char(Landsdel(i)),yyyy,cphy(i),L_Ncab(i))
+end
+end
---
+++ MetVed_Import_Buildingfiles.m
@@ -0,0 +1,98 @@
+function [Res,Cab] =  MetVed_Import_Buildingfiles()
+%--------------------------------------------------------------------------
+% Metved: Emissions from Residential Wood Combustion
+%--------------------------------------------------------------------------
+% Explanation goes here:
+% Options for global parameters set in MetVed_Control.
+% Lookup for residential files in folder. These are geoprocessed files that
+% come in the SSB grid and has been PreProcessed into shapefiles by the
+% preprocessing script: 
+%
+% NILU: Jan 2018: Henrik Grythe
+% NILU: Jun 2020: Henrik Grythe
+%--------------------------------------------------------------------------
+
+global ifiles tfiles Residentialpath CabinHousepath Emission_year
+global do_Residential do_Cabins use_temporary_files
+
+tf = cell(1,1);
+%--------------------------------------------------------------------------
+if do_Residential
+    ResFile = sprintf('%sAll_Dwellings_%04i',Residentialpath,Emission_year);
+    a = length(ifiles);
+    yearlim = 50;
+    i = 1;
+    while ~exist(strcat(ResFile,'.shp')) && i < yearlim
+        ResFile = sprintf('%sAll_Dwellings_%04i',Residentialpath,Emission_year-i);
+        i=i+1;
+    end
+    if i >= yearlim
+        fprintf('Year %i out of bounds\n', Emission_year)
+        Res =[];
+        return
+    end
+    if i>1; warning(sprintf('No Residential file for year %i \n using closest found %i ',Emission_year,Emission_year-i+1)); end
+    
+    if use_temporary_files
+        ce = split(ResFile,'/');
+        try
+            tf = strcat('Temp/',char(ce(end)),'.mat');
+            load(tf)
+            tfiles(1) ={tf}; 
+        catch
+            fprintf('No file found in Temp\n')
+            ifiles(a+1) = {ResFile};
+            fprintf('Reading large file ...')
+            Res = shaperead(ResFile);
+            fprintf('Done\n')
+            save(tf,'Res')
+        end
+    else
+        ifiles(a+1) = {ResFile};
+        fprintf('Reading large file ...')
+        Res = shaperead(ResFile);
+        fprintf('Done\n')
+    end
+
+else
+    Res =[];
+end
+%--------------------------------------------------------------------------
+if do_Cabins
+    CabFile = sprintf('%sCabins_Only_%04i',CabinHousepath,Emission_year);
+    a = length(ifiles);
+    yearlim = 50;
+    i = 1;
+    while ~exist(strcat(CabFile,'.shp')) && i < yearlim
+        CabFile = sprintf('%sAll_Dwellings_%04i',Residentialpath,Emission_year-i);
+        i=i+1;
+    end
+    if i >= yearlim
+        fprintf('Year %i out of bounds\n', Emission_year)
+        Res =[];
+        return
+    end
+    if i>1; warning(sprintf('No Residential file for year %i \n using closest found %i ',Emission_year,Emission_year-i+1)); end
+    
+    if use_temporary_files
+        ce = split(CabFile,'/');
+        try
+            tf = strcat('Temp/',char(ce(end)),'.mat');
+            load(tf)
+            tfiles(2) ={tf}; 
+        catch
+            fprintf('No file found in Temp\n')
+            ifiles(a+1) = {CabFile};
+            fprintf('Reading large file ...')
+            Cab = shaperead(CabFile);
+            fprintf('Done\n')
+            save(tf,'Cab')
+        end
+    end
+    
+else
+    Res =[];
+end
+
+
+end
