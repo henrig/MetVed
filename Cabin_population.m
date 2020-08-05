9b7a19b36b70888dbe1e25cbd358b96a4dc79385
Henrik Grythe <heg@heglux.nil0u.no>
2020-08-05 15:47:11 +0200

    added function to calculate the cabin load factor

---
+++ Cabin_population.m
@@ -0,0 +1,19 @@
+function Calendar = Cabin_population(date,Cabin_Population_file)
+% PARAMETERIZATION :
+
+% Daily Weight: time variation for norwegian traffic Norsk Regnesentral:
+% Saturday Applied as sunday, Friday and Sunday applied as Saturday.
+tt2      = 1./[0.27,0.07,0.05];
+cabindag = tt2/sum(tt2);
+
+Calendar = Find_Norwegian_Holidays(date,Cabin_Population_file);
+
+Twe = readtable(Cabin_Population_file,'Sheet','TrafficVolume');
+for i = 1:height(Calendar)
+    weigh(i,1) = Twe.SummerWeek(Calendar.Weeknum(i))*cabindag(Calendar.Cabin_day(i))*Calendar.Summer_Factor(i);
+    weigh(i,2) = Twe.WinterWeek(Calendar.Weeknum(i))*cabindag(Calendar.Cabin_day(i))*Calendar.Winter_Factor(i);    
+end
+
+Calendar.PopWeight_Summer = weigh(:,1); 
+Calendar.PopWeight_Winter = weigh(:,2);
+end
